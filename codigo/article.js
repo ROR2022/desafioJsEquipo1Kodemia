ae203416e13424bb93a6fa141571dafb170e549f
JulioVelazquezGit <vm.julio92@gmail.com>
2023-01-26 13:23:19 -0600

    revision de error in

--- codigo/article.js
+++ codigo/article.js
@@ -2,12 +2,15 @@ const URL_POST = 'https://desafiojsequipo1-default-rtdb.firebaseio.com/.json';
 
 const primerPost = {
     title:'Segundo Post favor',
-    content:'Post de Luis y Julio',
     urlImagePost: 'https://expertphotography.b-cdn.net/wp-content/uploads/2022/03/Background-Changer-App-Editing.jpg',
     urlImageUser: 'https://images.ctfassets.net/lh3zuq09vnm2/yBDals8aU8RWtb0xLnPkI/19b391bda8f43e16e64d40b55561e5cd/How_tracking_user_behavior_on_your_website_can_improve_customer_experience.png',
+    urlImageContent:'https://res.cloudinary.com/practicaldev/image/fetch/s--7jTKri2g--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://wweb.dev/weekly/content/124/indieworldwide.jpg" alt="Indie Worldwide',
     nameUser: 'primerUser',
     createdDate: '',
     likes: 0,
+    titleContent: '',
+    subTitleContent: '',
+    pContent: '',
   }
 
 const postArticle = async () => {
@@ -27,3 +30,9 @@ const postArticle = async () => {
 
 const pruebaPost = await postArticle()
 console.log(pruebaPost)
+
+const printArticle = (data) => {
+    const h1 = document.querySelector('h1');
+    h1.textContent = 'Please work, please work'
+    h1.appendChild(document);
+}
---
+++ codigo/articleJulio.js
@@ -0,0 +1,23 @@
+const URL = 'https://desafiojsequipo1-default-rtdb.firebaseio.com/';
+
+const mySearch = window.location.search;
+
+console.log('Hola desde articleJUlio')
+console.log(mySearch)
+const temp = new URLSearchParams(mySearch)
+const id = temp.get('id');
+console.log(id)
+
+const getData = async(id) => {
+   try {
+    const response = await fetch (`${URL}${id}.json`);
+    const result = await response.json();
+    console.log(result)
+    return result
+   } catch (error) {
+    console.log(error)
+    return error
+   }
+}
+
+getData(id)
